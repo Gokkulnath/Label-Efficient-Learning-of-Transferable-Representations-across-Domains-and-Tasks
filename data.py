854b8ebac62b91e3ed8e901e139ad85b46e20d5a
Gokkul Nath TS <Gokkulnath@users.noreply.github.com>
2019-02-27 20:52:14 +0530

    Added Custom SVNH

--- data.py
+++ data.py
@@ -7,35 +7,36 @@ from torch.tensor import Tensor as T
 
 # To Do:
 # Filter the Dataset Based on Range of Labels Given
-class FilteredMnist(MNIST):
-    """`MNIST <http://yann.lecun.com/exdb/mnist/>`_ Dataset.
-
-    Args:
-        root (string): Root directory of dataset where ``processed/training.pt``
-            and  ``processed/test.pt`` exist.
-        train (bool, optional): If True, creates dataset from ``training.pt``,
-            otherwise from ``test.pt``.
-        download (bool, optional): If true, downloads the dataset from the internet and
-            puts it in root directory. If dataset is already downloaded, it is not
-            downloaded again.
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.RandomCrop``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-
-        label_min : Minimun Value of Label That is to be allowed. 
+class CustomMNIST(MNIST):
+    """
+    Additional Args:
+        min_label : Minimun Value of Label That is to be allowed. 
         # Todo : Need to support list of values instead of min value
     """
-    def __init__(self, root, train=True, transform=None, target_transform=None, download=False,label_min=5):
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False,min_label=5):
         super().__init__(root, train=train, transform=transform, target_transform=target_transform, download=download)
-        self.label_range=label_min
-        self.train_data=self.train_data[self.train_labels>self.label_range]
-        self.train_labels=self.train_labels[self.train_labels>self.label_range]
+        self.min_label=min_label
+        self.train_data=self.train_data[self.train_labels>=self.min_label]
+        self.train_labels=self.train_labels[self.train_labels>=self.min_label]
 
 
 # Checked And working as expected
 # mnist=FilteredMnist('.',download=True,label_min=5)
 # print((mnist.train_labels))
 
+class CustomSVNH(SVHN):
+    def __init__(self, root, split='train', transform=None, target_transform=None, download=False,max_label=5):
+        super().__init__(root, split=split, transform=transform, target_transform=target_transform, download=download)
+        self.max_label=max_label
+        self.data=self.data[self.labels<=self.max_label]
+        self.labels=self.labels[self.labels<=self.max_label]
 
 
+svnh=CustomSVNH(root='.',download=True,max_label=4)
+mnist=CustomMNIST(root='.',download=True,min_label=5)
+
+
+print(svnh.data.shape,svnh.labels.shape)
+#print(np.unique(svnh.labels))
+#print(np.unique(mnist.train_labels))
+print(mnist.train_data.shape,mnist.train_labels.shape)
