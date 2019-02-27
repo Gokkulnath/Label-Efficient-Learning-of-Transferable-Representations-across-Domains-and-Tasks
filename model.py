68db0d4e8bf21b41b38f5c5da6f2f51966718568
Gokkul Nath TS <Gokkulnath@users.noreply.github.com>
2019-02-27 22:32:10 +0530

    Day 1 : initial commit for model and utils.py

---
+++ model.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+import torch.optim as optim
+
+class MyNetwork(nn.Module):
+    
+    def __init__():
+        pass
---
+++ utils.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import torch
+
+def softmax_temperature():
+    pass 
+    
